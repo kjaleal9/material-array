8e745b5ef447bd11bc1d37db1a76876ef25c2b49
Kris Leal <kjaleal9@gmail.com>
2023-04-07 20:57:32 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Matrix from './Matrix';
+import { Grid, Paper } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container sx={{ backgroundColor: '#2196f3',height:'100vh' }}>
+      <Grid item xs={12} sm={8} md={6} xl={4}>
+        <Paper sx={{margin:'1em',height:'auto',overflow:'none'}}>
+          <Matrix/>
+        </Paper>
+      </Grid>
+    </Grid>
   );
 }
 
